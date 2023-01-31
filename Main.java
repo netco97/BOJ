94c2a410de29f7450da842245d83ea6b3fc92cff
Sangmin <101931428+netco97@users.noreply.github.com>
2023-01-31 18:05:20 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack= new Stack<>();
+        int n = Integer.parseInt(br.readLine());
+        int num, sum=0;
+
+        for(int i=0; i<n;i++)
+        {
+            num = Integer.parseInt(br.readLine());
+            if(num == 0)
+            {
+                stack.pop();
+            }
+            else{
+                stack.push(num);
+            }
+        }
+
+        for(int i=0;i<stack.size();i++)
+        {
+            sum += stack.elementAt(i);
+        }
+        System.out.println(sum);
+
+    }
+}
