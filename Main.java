058a099982d152de88d54a309edcce118b3bfe8f
Sangmin <101931428+netco97@users.noreply.github.com>
2023-01-31 17:44:20 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        String str_arr[] = new String[n];
+        String answer[] = new String[m];
+        int count = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            str_arr[i] = br.readLine();
+        }
+        for(int i=0;i<m;i++)
+        {
+            answer[i] = br.readLine();
+            for(int j=0;j<n;j++)
+            {
+                if(answer[i].equals(str_arr[j]))
+                    count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
