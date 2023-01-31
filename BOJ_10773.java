adfe1e0603f3c4d5b0652af70db396f014edc169
Sangmin <101931428+netco97@users.noreply.github.com>
2023-01-31 18:05:50 +0900

    Update and rename Main.java to BOJ_10773.java

--- BOJ_10773.java
+++ BOJ_10773.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class Main {
+public class BOJ_10773 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Stack<Integer> stack= new Stack<>();
