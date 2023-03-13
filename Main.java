e5f0efdb258ad4907ed7e0cf9de7016ed7f8bde3
Sangmin <101931428+netco97@users.noreply.github.com>
2023-03-13 14:42:26 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String n = br.readLine();
+        StringTokenizer st = new StringTokenizer(n);
+        int n_hour = Integer.parseInt(st.nextToken());
+        int n_minute = Integer.parseInt(st.nextToken());
+
+        int m = Integer.parseInt(br.readLine());
+
+        n_minute += m;
+        int n_remain = n_minute/60;
+        if(n_minute>=60)
+        {
+            n_hour += n_remain;
+            n_minute -= 60*(n_remain);
+        }
+
+        if(n_hour>=24)
+        {
+            n_hour = n_hour-24;
+        }
+        System.out.println(n_hour+" "+n_minute);
+
+
+    }
+}
+
+/*import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n-543);
+
+    }
+}*/
