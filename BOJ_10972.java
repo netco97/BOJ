f3e741f328a0eb2cfa5cbc8da669448eebeb41b8
Sangmin <101931428+netco97@users.noreply.github.com>
2023-06-19 16:27:09 +0900

    [BOJ]/10972/S3/1시간

---
+++ BOJ_10972.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_10972 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static int[] nums;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        nums = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        for(int i=0;i<N;i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        if (nextPermutation()) {
+            for (int i = 0; i < N; i++) {
+                System.out.print(nums[i] + " ");
+            }
+        } else {
+            System.out.println("-1");
+        }
+    }
+
+    private static boolean nextPermutation() {
+        int i = nums.length - 1;
+        //1 2 3 4 5 6
+        while (i > 0 && nums[i - 1] >= nums[i])  i--;
+        if (i <= 0) return false;
+
+        int j = nums.length - 1;
+        while (nums[j] <= nums[i - 1])  j--;
+
+        swap(i - 1, j);
+        j = nums.length - 1;
+        while (i < j) {
+            swap(i, j);
+            i++; j--;
+        }
+        return true;
+    }
+
+    private static void swap(int idx1, int idx2) {
+        int temp = nums[idx1];
+        nums[idx1] = nums[idx2];
+        nums[idx2] = temp;
+    }
+}
